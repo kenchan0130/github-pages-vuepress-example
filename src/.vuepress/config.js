32bdb9d871c798ba24d6510d09178811b58f5b67
Tadayuki Onishi <tt.tanishi100@gmail.com>
2018-06-17 23:22:21 +0900

    directly set dest option

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,5 +4,5 @@ const description = 'Just play VuePress for kenchan0130 blog.';
 module.exports = {
   title: title,
   description: description,
-  dest: process.env.npm_package_destDirectory
+  dest: 'docs/'
 };
